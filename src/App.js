b655051fa4107259e3357c15070488b6871b2e89
realwixi <98758831+realwixi@users.noreply.github.com>
2023-09-10 08:57:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoWrapper } from './Todocomp/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper  />
+    </div>
+  );
+}
+
+export default App;
